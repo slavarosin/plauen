67d0154c64275420fb850b0e3332b7665c247d8f
slavarosin <slavarosin@gmail.com>
2015-06-16 23:53:32 +0200

    Removed binary files

--- docs/install.sql
+++ docs/install.sql
@@ -1,6 +1,3 @@
-INSERT INTO `users` (`id`,`login`,`passwd`,`first_name`,`last_name`,`email`,`phone`,`role`) VALUES
- (1,'administrator','9e87b917259bc9781b99af870891e002','Admin','Root','admin@gobro.net','+372','ADMIN');
-
 insert into `score_rules` (`id`, `country`, `price`, `score`) values (1, 'EE', '5', 5);
 insert into `score_rules` (`id`, `country`, `price`, `score`) values (2, 'EE', '10', 12);
 insert into `score_rules` (`id`, `country`, `price`, `score`) values (3, 'EE', '15', 21);
